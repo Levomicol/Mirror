a5be077100b1415cf35ea5807250f2cc5085d37c
Пользователь <levomicol>
2025-06-06 07:58:20 +0300

    Second commit

--- Mirror output/Output_Format.py
+++ Mirror output/Output_Format.py
@@ -1,21 +1,29 @@
+import logging
+
 #- Есть некий список слов перебрать его исключая повторяющиеся;
 #- Прописать алгоритм поиска слов полиномов(т.е. читающихся одинаково с обеих сторон);
 #- Вывести строки в консоль задом на перёд, строки-полиномы заменить строкой "полином".
 
-list1 = ["ото", "этот", "эта", "эту", "тот", "этот"]
+logging.basicConfig(filename="gfg-log.log", filemode="w", level=logging.DEBUG, format="%(name)s → %(levelname)s: %(message)s")
+logger = logging.getLogger()
 
+FileOutputHandler = logging.FileHandler('Messages.log')
+
+logger.addHandler(FileOutputHandler)
+
+list1 = ["ото", "этот", "эта", "эту", "тот", "этот"]
 s = list(set(list1))
 
 for i in s:
     reversed_i = i[::-1]
     
     if i == reversed_i:
-        print("Полином!")
+           print("Полином!")
     else:
-       string = [i]
-       for st in string:
-           reversed_st = st[::-1]
-           print(reversed_st)
-
+          string = [i]
+          for st in string:
+             reversed_st = st[::-1]
+             print(reversed_st)
+             logger.debug(reversed_st)
 
 
